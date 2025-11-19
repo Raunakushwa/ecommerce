549d46580bcdbd68d9a69a95262615208e09dc55
raunak <raunakush00@gmail.com>
2025-11-19 16:08:49 +0530

    added backend

---
+++ backend/controllers/userController.js
@@ -0,0 +1,104 @@
+import userModel from '../models/userModel.js';
+import validator from 'validator';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+
+const createToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET);
+}
+
+//create account or login
+
+//Route for user login
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await userModel.findOne({ email });
+
+        if (!user) {
+            return res.json({ success: false, message: "User not found" });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (isMatch) {
+            const token = createToken(user._id);
+            res.json({ success: true, message: token });
+        }
+        else {
+            res.json({ success: false, message: "Invalid password" });
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message });
+    }
+}
+
+
+//Route for user register
+
+const registerUser = async (req, res) => {
+
+    try {
+        const { name, email, password } = req.body;
+
+        //checking if user already exists
+        const exists = await userModel.findOne({ email });
+        if (exists) {
+            return res.json({ success: false, message: "user already exists" })
+        }
+
+        //validating email format and strong password
+
+        if (!validator.isEmail(email)) {
+            return res.json({ success: false, message: "invalid email format" })
+        }
+        if (password.length < 8) {
+            return res.json({ success: false, message: "Please enter a strong password" })
+        }
+
+        //hashing password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new userModel({
+            name,
+            email,
+            password: hashedPassword
+        })
+
+        const user = await newUser.save();
+        const token = createToken(user._id);
+
+        res.json({ success: true, message: token })
+
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message })
+    }
+
+
+}
+
+//Route for admin login
+
+const adminLogin = async (req,res) => {
+    try {
+
+        const { email, password } = req.body;
+        if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
+            const token = jwt.sign(email + password, process.env.JWT_SECRET);
+            res.json({ success: true, token })
+        }
+        else {
+            res.json({ success: false, message: "Invalid Credentials" });
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message })
+    }
+}
+
+export { loginUser, registerUser, adminLogin };
